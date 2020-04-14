4bd818d8df45970d51e67e97b6ef6c87d36f8615
Darwin <darwin78913@gmail.com>
2020-04-14 22:38:45 +0000

    added server

---
+++ server/main.py
@@ -0,0 +1,56 @@
+from flask import Flask, render_template
+from flask_mqtt import Mqtt
+from flask_socketio import SocketIO
+import time
+import os
+
+app = Flask(__name__)
+app.config['MQTT_BROKER_URL'] = os.getenv("MQTT_BROKER_URL")  
+app.config['MQTT_BROKER_PORT'] = int(os.getenv("MQTT_PORT"))
+app.config['MQTT_USERNAME'] = os.getenv("MQTT_USER")  
+app.config['MQTT_PASSWORD'] = os.getenv("MQTT_PASSWD")   
+app.config['MQTT_KEEPALIVE'] = 10  # set the time interval for sending a ping to the broker to 5 seconds
+app.config['MQTT_TLS_ENABLED'] = False  # set TLS to disabled for testing purposes
+
+mqtt = Mqtt(app)
+socketio = SocketIO(app)
+
+@mqtt.on_connect()
+def handle_connect(client, userdata, flags, rc):
+    # mqtt.subscribe('To Photon')
+    print("[CONNECTION]: MQTT Connected!")
+
+@mqtt.on_message()
+def handle_mqtt_message(client, userdata, message):
+    data = dict(
+        topic=message.topic,
+        payload=message.payload.decode()
+    )
+    print(data)
+
+def change_colors(chip_id, color):
+    mqtt.publish(chip_id, color);
+
+@app.route('/')
+def base():
+    # if "change_color" in request.form:
+    #     mqtt.publish("To Chips", msg)
+    return render_template('index.html')
+
+@socketio.on("lights")
+def change_lights_message(message):
+    print("[LIGHTS] : " + str(message))
+    change_colors(message["id"], message["color"])
+
+@socketio.on("info")
+def print_debug(message):
+    print("[INFO]: " + str(message))
+
+@socketio.on("connect")
+def on_connect():
+    print("[CONNECTION]: Socket connected!")
+
+
+
+if __name__ == '__main__':
+    socketio.run(app, host='0.0.0.0')
---
+++ server/static/js/mqtt_topic_as_id.js
@@ -0,0 +1,118 @@
+// BOARD INFO
+var rows = 3;
+var cols = 3;
+var lights = [0,0,0,0,0,0,0,0,0];
+
+// WEBSOCKET INFO
+var connected = false;
+const socket = io();
+
+
+// MISC INFO
+var red = "#D8554B";
+var green = "#64B743"
+var blue = "#4BAED8";
+
+// COLOR WHEEL STUFF
+var colorWheel = new iro.ColorPicker("#colorWheel", {
+
+    layout: [
+    { 
+      component: iro.ui.Wheel,
+      options: {
+        wheelLightness: true,
+        wheelAngle: 0,
+        wheelDirection: "anticlockwise"
+      } 
+    },
+    {
+      component: iro.ui.Box,
+      options: {
+        // see below
+      }
+    },
+    {
+      component: iro.ui.Slider,
+      options: {
+        sliderType: 'hue', // can also be 'saturation', 'value', 'alpha' or 'kelvin',
+        sliderShape: 'circle'
+      }
+    }
+    ]
+  
+});
+
+// CONVERT AN RGB ARRAY OF NUMBERS TO AN ARRAY OF STRINGS WHICH EACH ONE CORRESPONDING TO THAT LIGHT
+function number_to_color_array(number_array) {
+    var color_array = [];
+    for(var i = 0; i < (rows * cols); i++) {
+        switch(number_array[i]) {
+            case 0:
+                color_array.push("000000");
+                break;
+            case 1:
+                color_array.push("ff0000");
+                break;
+            case 2:
+                color_array.push("00ff00");
+                break;
+            case 3:
+                color_array.push("0000ff");
+                break;
+        }
+    }
+
+    return color_array;
+}
+
+socket.on('connect', function() {
+    socket.emit("info", "Socket Client Connected!");
+    connected = true;
+});
+
+
+
+function clicked(id) {
+    var finalMessage = "";
+    switch(lights[id]) {
+        case 0:
+            lights[id] = 1;
+            document.getElementById("button-"+id).style.background = red;
+            break;
+        case 1:
+            lights[id] = 2;
+            document.getElementById("button-"+id).style.background = green;
+            break;
+        case 2:
+            lights[id] = 3;
+            document.getElementById("button-"+id).style.background = blue;
+            break;
+        default:
+            lights[id] = 0;
+            document.getElementById("button-"+id).style.background = "white";
+            break;
+    }
+
+    colorsArray = number_to_color_array(lights);
+    console.log(colorsArray);
+    // console.log("Sent: " + lights);
+    if(connected == true) {
+        console.log("Button Clicked");
+        for(var i = 0; i < colorsArray.length; i++) {
+            console.log("In array loop");
+            var id = i.toString().padStart(3, '0');
+            socket.emit("lights", {"id": id, "color": colorsArray[i]});
+            // client.subscribe(i.toString().padStart(3, '0')); // TOPIC
+            // message = new Paho.MQTT.Message(colorsArray[i]); // MESSAGE
+            // message.destinationName = i.toString().padStart(3, '0'); //TOPIC
+            console.log("Sending message to topic: " + id);	
+            // document.getElementById("messages").innerHTML += '<br><span><b> Sending Message To Topic:</b><br> ' + id + '</span>';
+            // client.send(message);
+        }
+        
+    } else {
+        location.reload();
+    }
+}
+
+
